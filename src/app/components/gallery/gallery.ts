5b53d4841d6f5dedc1909ead6b55fd8f261d6c85
ZUXROK <shotamuradashvili492@gmail.com>
2025-07-20 16:49:09 +0400

    Removed text from the GALLERY page

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -12,17 +12,14 @@ export class GalleryComponent {
   galleryItems = [
     {
       title: 'LIFEREALM (0.1.1)',
-      description: 'Discover LIFEREALM with its newly updated visuals. The game now features a more vibrant environment, detailed landscapes, and enhanced lighting, offering a truly immersive visual experience.',
       imageUrl: 'assets/Picture_3.png'
     },
     {
       title: 'LIFEREALM (0.1.0)',
-      description: 'Explore the early stages of LIFEREALM, a game designed to showcase beautiful and immersive worlds with vibrant terrains, lush grass, and stunning skies.',
       imageUrl: 'assets/Picture_2.png'
     },
     {
       title: 'Game-1 (0.1.0)',
-      description: 'Take a first look at my debut Unity game! This initial version showcases a vibrant terrain, lush trees, and an early encounter with the bear.',
       imageUrl: '/assets/Picture_1.png'
     }
   ];
